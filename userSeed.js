96150bfe2ff2b376efe738164ca87260ce8b89c9
Mayank <mayank.semwalji@gmail.com>
2025-05-01 00:53:57 +0530

    db changed

--- userSeed.js
+++ userSeed.js
@@ -3,8 +3,9 @@ import bcrypt from 'bcrypt'
 import connectToDatabase from './db/db.js'
 
 const userRegister = async () => {
-    connectToDatabase()
+    
    try {
+        await connectToDatabase();
     const hashPassword = await bcrypt.hash("admin", 10)
         const newUser = new User({
             name: "Admin",
