b0176d133ceeb50b0deeca81062700acc1898745
Mayank <mayank.semwalji@gmail.com>
2025-04-30 23:08:41 +0530

    url changed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import connectToDatabase from './db/db.js'
 connectToDatabase()
 const app = express()
 app.use(cors({
-    origin:  "http://localhost:5173", 
+    origin:  "https://employee-frontend-juua.vercel.app", 
     credentials: true,
 }))
 app.use(express.json())
