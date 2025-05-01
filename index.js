d8bd078a2159a24930982667fb278e7dc23ec0db
Mayank <mayank.semwalji@gmail.com>
2025-05-01 21:54:17 +0530

    add

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 connectToDatabase()
 app.use(cors({
-    origin:"https://employee-frontend-juua-1hmdeshe3.vercel.app/login", 
+    origin:"https://employee-frontend-juua.vercel.app", 
     credentials: true,
 }))
 app.use(express.json())
